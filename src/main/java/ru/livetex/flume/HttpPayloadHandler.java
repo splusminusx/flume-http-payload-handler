d35b5f0c909589411fe08e3e759f5ce2715be211
stx <splusminusx@gmail.com>
2015-02-08 21:15:38 +0300

    Http Payload Handler

---
+++ src/main/java/ru/livetex/flume/HttpPayloadHandler.java
@@ -0,0 +1,41 @@
+package ru.livetex.flume;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.event.EventBuilder;
+import org.apache.flume.source.http.HTTPSourceHandler;
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * Обработчик тела HTTP запроса.
+ */
+public class HttpPayloadHandler implements HTTPSourceHandler {
+
+    @Override
+    public void configure(Context context) {
+    }
+
+    /**
+     * Порождение событий из HTTP запроса.
+     *
+     * @param request - запрос.
+     * @return - список событий.
+     * @throws Exception
+     */
+    public List<Event> getEvents(HttpServletRequest request) throws Exception {
+        BufferedReader reader = request.getReader();
+        int contentLength = request.getContentLength();
+        char[] buffer = new char[contentLength];
+
+        reader.read(buffer);
+
+        List<Event> eventList = new ArrayList<Event>(0);
+        eventList.add(EventBuilder.withBody(new String(buffer).getBytes()));
+
+        return eventList;
+    }
+}
