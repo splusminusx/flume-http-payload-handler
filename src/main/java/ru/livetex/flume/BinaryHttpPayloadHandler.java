d167f3780c3faf754f8a6d452afef0b1e936b0ee
werschinin <werschinin@gmail.com>
2015-10-09 17:09:05 +0300

    fixes

--- src/main/java/ru/livetex/flume/BinaryHttpPayloadHandler.java
+++ src/main/java/ru/livetex/flume/BinaryHttpPayloadHandler.java
@@ -3,7 +3,7 @@ package ru.livetex.flume;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
 
-public class BinaryHttpPayloadHandler  extends HttpPayloadHandler {
+public class BinaryHttpPayloadHandler extends HttpPayloadHandler {
     private static final TProtocolFactory protocolFactory = new TBinaryProtocol.Factory();
 
     @Override
