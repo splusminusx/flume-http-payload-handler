d32b42d3dc2090f5ec81edd6ff6942e8f88d799a
werschinin <werschinin@gmail.com>
2015-10-27 11:18:12 +0300

    add builders for interceptors

--- src/main/java/ru/livetex/flume/BinaryThriftMethodInterceptor.java
+++ src/main/java/ru/livetex/flume/BinaryThriftMethodInterceptor.java
@@ -1,9 +1,22 @@
 package ru.livetex.flume;
 
+import org.apache.flume.Context;
+import org.apache.flume.interceptor.Interceptor;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
 
 public class BinaryThriftMethodInterceptor extends ThriftMethodInterceptor {
+    public static class Builder implements Interceptor.Builder {
+        @Override
+        public Interceptor build() {
+            return new BinaryThriftMethodInterceptor();
+        }
+
+        @Override
+        public void configure(Context context) {
+        }
+    }
+
     private static final TProtocolFactory protocolFactory = new TBinaryProtocol.Factory();
 
     @Override
--- src/main/java/ru/livetex/flume/JsonThriftMethodInterceptor.java
+++ src/main/java/ru/livetex/flume/JsonThriftMethodInterceptor.java
@@ -1,10 +1,23 @@
 package ru.livetex.flume;
 
 
+import org.apache.flume.Context;
+import org.apache.flume.interceptor.Interceptor;
 import org.apache.thrift.protocol.TJSONProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
 
 public class JsonThriftMethodInterceptor extends ThriftMethodInterceptor {
+    public static class Builder implements Interceptor.Builder {
+        @Override
+        public Interceptor build() {
+            return new JsonThriftMethodInterceptor();
+        }
+
+        @Override
+        public void configure(Context context) {
+        }
+    }
+
     private static final TProtocolFactory protocolFactory = new TJSONProtocol.Factory();
 
     @Override
